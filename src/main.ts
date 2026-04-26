4308fce12dd202bede76b13595a542bc596e3fca
EmreTopcu04 <info.etopcu@gmail.com>
2026-04-26 14:06:33 +0300

    chore: remove legacy local storage cleanup from main entry point

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import './assets/main.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import { cleanupLocalStorage } from './utils/cleanup'
-
-// Clean up old storage data before starting
-cleanupLocalStorage()
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
